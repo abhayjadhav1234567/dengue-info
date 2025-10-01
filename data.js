1c05b810d0ba525a2eb142efd193ed048a0947f8
abhayjadhav1234567 <anj4591217@gmail.com>
2025-10-01 15:12:57 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+// Example dengue cases by country (sample data)
+const dengueData = [
+  { country: "India", cases: 1500000, lat: 20.5937, lon: 78.9629 },
+  { country: "Brazil", cases: 1200000, lat: -14.235, lon: -51.9253 },
+  { country: "Indonesia", cases: 950000, lat: -0.7893, lon: 113.9213 },
+  { country: "Philippines", cases: 800000, lat: 12.8797, lon: 121.774 },
+  { country: "Thailand", cases: 700000, lat: 15.87, lon: 100.9925 },
+  { country: "Mexico", cases: 600000, lat: 23.6345, lon: -102.5528 },
+  { country: "Sri Lanka", cases: 350000, lat: 7.8731, lon: 80.7718 }
+];
